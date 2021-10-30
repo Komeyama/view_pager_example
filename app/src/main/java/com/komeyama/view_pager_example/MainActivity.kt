6d4c4b87e88ca3f559119aefb14148da0dc70c3c
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2021-10-30 21:36:55 +0900

    add: custom tab layout

--- app/src/main/java/com/komeyama/view_pager_example/MainActivity.kt
+++ app/src/main/java/com/komeyama/view_pager_example/MainActivity.kt
@@ -2,6 +2,9 @@ package com.komeyama.view_pager_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -25,10 +28,36 @@ class MainActivity : AppCompatActivity() {
         val pagerAdapter = PagerAdapter(this)
         binding.pager.adapter = pagerAdapter
         TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
-            tab.text = tabList[position]
+            tab.setCustomView(R.layout.custom_tab)
+            val tabViewText = tab.view.findViewById<TextView>(R.id.tab_text)
+            tabViewText.text =
+                ("${resources.getString(R.string.tab_name)} ${position + 1}").toString()
+
+            val tabViewImage = tab.view.findViewById<ImageView>(R.id.tab_image)
+            when (position) {
+                0 -> tabViewImage.setImageResource(R.drawable.ic_baseline_create_24)
+                1 -> tabViewImage.setImageResource(R.drawable.ic_baseline_email_24)
+                2 -> tabViewImage.setImageResource(R.drawable.ic_baseline_date_range_24)
+            }
         }.attach()
 
-        binding.pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {})
+        binding.pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                for (tabID in 0..tabList.size) {
+                    if (tabID == position) continue
+                    setTabTextColor(tabID, R.color.white)
+                }
+                setTabTextColor(position, R.color.purple_200)
+            }
+        })
+    }
+
+    private fun setTabTextColor(position: Int, colorID: Int) {
+        binding.tabLayout.getTabAt(position)?.customView?.findViewById<TextView>(R.id.tab_text)
+            ?.setTextColor(ContextCompat.getColor(applicationContext, colorID))
     }
 
     private inner class PagerAdapter(f: FragmentActivity) : FragmentStateAdapter(f) {
